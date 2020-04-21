ec47e435f5855cf36f70d3ad65df7f0f11755536
pratibha_hankare <pratibha1hankare@gmail.com>
2020-04-21 23:03:06 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace whileIteration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool displayMeanu = true;
+            while (displayMeanu)
+            {
+                MainMenu();
+            }
+        }
+
+        public static bool MainMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("Choose an option:");
+            Console.WriteLine("1) Print Number Game");
+            Console.WriteLine("2) Guessing Game");
+            Console.WriteLine("3) Exit");
+            String result = Console.ReadLine();
+            if (result == "1")
+            {
+                PrintNumberGame();
+                return true;
+            }else if (result == "2")
+            {
+                GuessTheNumberGame();
+                return true;
+            }
+            else
+            {
+                return false;
+            } 
+        }
+
+        public static void PrintNumberGame()
+        {
+            Console.Clear();
+            Console.Write("Print Number!");
+            Console.Write("Type Number!");
+            int result = int.Parse(Console.ReadLine());
+            int counter = 1;
+            while (counter < result+1)
+            {
+                Console.Write(counter);
+                Console.Write("-");
+                counter++;
+            }
+            Console.ReadLine();
+        }
+
+        public static void GuessTheNumberGame()
+        {
+            Console.Clear();
+            Console.WriteLine("Guess the Number!!!");
+            Random randomNumber = new Random();
+            int randomNumberValue = randomNumber.Next(1,11);
+
+            int guessCount = 0;
+            bool inCorrectAns = true;
+            do
+            {
+                Console.WriteLine("Guess the Value");
+                string readGuessedValue = Console.ReadLine();
+                guessCount++;
+                if (randomNumberValue.ToString() == readGuessedValue)
+                    inCorrectAns = false;
+                else
+                    Console.Write("Wrong guess!!!");
+            } while (inCorrectAns);
+            Console.Write("Correct guess!!!.It took you {0} to GUESS Correct Number.", guessCount);
+            Console.ReadLine();
+        }
+    }
+}
